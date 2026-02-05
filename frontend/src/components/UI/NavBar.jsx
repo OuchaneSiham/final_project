4e29412a859cbaac1087851039fb8937380d699b
changli2001 <changlichan2@gmail.com>
2026-02-05 16:39:50 +0100

    adding the ProfileIcone component for the navBar button still discaticated

--- frontend/src/components/UI/NavBar.jsx
+++ frontend/src/components/UI/NavBar.jsx
@@ -1,14 +1,14 @@
 import Logo from "./Logo";
-
+import ProfileIcone from "./ProfileIcone";
 
 export default function Navbar()
 {
     return (
-        <nav className="flex items-center justify-between w-full h-20 border mt-[3] rounded-[19px] border-[#C77966]/20 bg-[#4A302A] mb-8 ">
+        <nav className="flex items-center justify-between w-full p-5 h-20 border mt-[3] rounded-[19px] border-[#C77966]/20 bg-[#4A302A] mb-8 ">
             <div className="flex-1"/>
             <Logo variant ='Logo'/>
-            <div className="flex-1">
-                
+            <div class="flex-1 flex justify-end gap-4">
+                <ProfileIcone />
             </div>
         </nav>
     );
--- frontend/src/components/UI/ProfileIcone.jsx
+++ frontend/src/components/UI/ProfileIcone.jsx
@@ -1,1 +1,14 @@
-import face from '../../../assets/images/face.jpeg';
+import face from '/Users/ochangli/Desktop/tictactoe/frontend/src/assets/images/face.jpg';
+
+
+export default function ProfileIcone({className})
+{
+    return (
+        <button className='h-12 w-12 rounded-full overflow-hidden'>
+            <img   src={face}
+                alt="testing img"
+                className='h-full w-full object-cover'
+                />
+        </button>
+    );
+}
