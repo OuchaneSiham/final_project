245f9f42cc6b0cafb104864a920b689bca7d1399
changli2001 <changlichan2@gmail.com>
2026-02-05 23:00:25 +0100

    making the profile icone animated when Hovering

--- frontend/src/components/UI/ProfileIcone.jsx
+++ frontend/src/components/UI/ProfileIcone.jsx
@@ -4,7 +4,7 @@ import face from '/Users/ochangli/Desktop/tictactoe/frontend/src/assets/images/f
 export default function ProfileIcone({className, })
 {
     return (
-        <button className='h-12 w-12 rounded-full overflow-hidden'>
+        <button className='h-12 w-12 rounded-full overflow-hidden hover:scale-125 transition duration-700 ease-in-out'>
             <img   src={face}
                 alt="testing img"
                 className='h-full w-full object-cover'></img>
