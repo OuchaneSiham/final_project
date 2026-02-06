4203438fea47d2be257e98809cac51627f36f31d
changli2001 <changlichan2@gmail.com>
2026-02-06 02:33:47 +0100

    changing the Background color of the DropDown

--- frontend/src/components/UI/DropDown.jsx
+++ frontend/src/components/UI/DropDown.jsx
@@ -3,7 +3,7 @@
 export default function DropDown()
 {
     return (
-        <div className="absolute w-45 h-50 bg-gray-200 top-[110%] right-[50%] rounded-2xl">
+        <div className="absolute w-45 h-50 bg-[#D3A376] top-[110%] right-[50%] rounded-2xl">
     
         </div>
     );
