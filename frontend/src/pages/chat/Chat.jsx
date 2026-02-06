ef5d04fa2ac69b39f48073653810f66c068b0fad
changli2001 <changlichan2@gmail.com>
2026-02-06 19:47:08 +0100

    adding a new section to displey the User meta data

--- frontend/src/pages/chat/Chat.jsx
+++ frontend/src/pages/chat/Chat.jsx
@@ -10,9 +10,9 @@ export default function Chat()
             
             < Navbar />
             
-            <div className="flex flex-1 gap-4 min-h-0">
+            <div className="flex flex-1 gap-4 min-h-0 px-3">
                 {/* LEFT SIDEBAR */}
-                <div className="flex flex-col w-[350px] rounded-3xl gap-5">
+                <div className="flex flex-col w-[350px] rounded-3xl gap-5 mr-5">
                     {/* top chat Bar */}
                     <div className="flex items-center justify-start w-auto h-[92px] bg-blue-200 rounded-l-2xl rounded-t-2xl p-4 gap-4">
                         <div className='h-11 w-11 rounded-full transition duration-350 ease-in-out overflow-hidden'>
@@ -30,7 +30,24 @@ export default function Chat()
                         <Contacts/>
                         <Contacts/>
                     </div>
-                </div>        
+                </div>
+                
+                {/*Right Side Bar*/}
+                <div className="flex flex-1 gap-10">
+                    <div className="w-2/3 bg-gray-500 rounded-3xl ">
+
+                    </div>
+
+                    {/*most right bar for user infos */}
+                    <div className="flex flex-col w-1/3  bg-red-200 my-30  rounded-[30px] items-center pt-40">
+                        <div className='h-50 w-50 rounded-full transition duration-350 ease-in-out overflow-hidden'>
+                            <img   src={face}
+                                    alt="testing img"
+                                    className='h-full w-full object-cover'>
+                            </img>
+                        </ div>
+                    </div>
+                </div>
             </div>
 
         </div>
