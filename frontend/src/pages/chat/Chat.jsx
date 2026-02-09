092cd922f4c560854fe3e4bdc27648252ddae794
ochangli2001 <changlichan2@gmail.com>
2026-02-09 20:19:05 +0100

    fixing the displaying User at the Header of conversation depending on hte Userid , issue still when the Chat compo get rendred for the first-time

--- frontend/src/pages/chat/Chat.jsx
+++ frontend/src/pages/chat/Chat.jsx
@@ -9,14 +9,10 @@ import FriendAvt from '../../components/chat/friendAvatar.jsx';
 
 export default function Chat()
 {
-    const [selectedUserId, setSelectedUserId] = useState(null);
+    const [selectedUserId, setSelectedUserId] = useState(0);
     const [friends, setFriends] = useState(mockFriends);
 
-    const handleUserClick = (user) => {
-        setSelectedUserId(user.id);
-        // Here you can also fetch messages for this user
-        console.log(`Selected user: ${user.id}`);
-      };
+    console.log(selectedUserId);
     return(
             <div className="flex flex-col items-center min-h-screen bg-gradient-to-br from-[#3B2F2F] via-[#7E5C4A] to-[#F2D7B6] py-4 gap-4 px-6">
                 <Navbar/>
@@ -25,39 +21,39 @@ export default function Chat()
                 </p>
                 {/* Global box chat container */}
                 <div className="flex-1 flex gap-4 max-w-7xl h-full w-full bg-gray-500 rounded-3xl shadow-xl py-5 px-4">
+                    
                     {/* side Bar*/}
                     <div className="flex flex-col items-center max-w-7xl w-24 border-r bg-gradient-to-br from-rose-50 to-orange-50 p-4 gap-3 overflow-y-auto rounded-2xl border-slate-300">
                         {friends.map(
                             (user) => (
-                                <FriendAvt onClick={() => handleUserClick(user)}
-                                            friend={user}/>
+                                <FriendAvt onClick={() => setSelectedUserId(user.id)}
+                                    key={user.id}        
+                                    friend={user}/>
                             )
                         )}
                     </div>
+
                      {/*Converstaion*/}
                     <div className="flex flex-1 flex-col w-full bg-gray-200 rounded-2xl pt-6">
                         {/* Header section */}
                         <div className=" flex items-center gap-3 px-6 py-4 pb-7 border-b  border-red-400">
                             <img 
-                                src={friends[0].userFace} 
-                                alt={friends[0].userName}
+                                src={friends[selectedUserId].userFace}
+                                alt={friends[selectedUserId].userName}
                                 className="w-14 h-14 rounded-full object-cover ring-2 ring-white"
                             />
                             <div className="flex-1">
                                 <h3 className="text-white font-semibold text-lg">
-                                    Ochangli
+                                    {friends[selectedUserId].userName}
                                 </h3>
                             </div>
                         </div>
                         {/* Messages Section */}
                         <div className="flex-1 bg-green-500 space-y-4 overflow-y-auto px-6 py-4 ">
-
                         </div>
                         <div className="px-6 py-4 bg-gray-600 border-t border-gray-400 flex items-center gap-3">
                             <input type="text" placeholder="Type a message..."className=" flex-1 px-4 py-3 rounded-full bg-gray-700 text-white placeholder-gray-400focus:outline-none focus:ring-2 focus:ring-amber-500"/>
-                            <button className="w-12 h-12 rounded-full bg-amber-500 hover:bg-amber-600 text-white flex items-center justify-centertransition-allhover:scale-105
-  ">
-    </button>
+                            <button className="w-12 h-12 rounded-full bg-amber-500 hover:bg-amber-600 text-white flex items-center justify-centertransition-allhover:scale-105"></button>
                         </div>
                     </div>
                 </div>
