c10c75c22e8aa1637b8d740a199d5c214bd4455b
changli2001 <changlichan2@gmail.com>
2026-02-12 02:30:17 +0100

    mergin Landing Page , signin and signUP pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,19 @@ import Chat from "./pages/chat/Chat";
 import Landing from "./pages/LandingPage";
 import Friend from "./pages/Friends";
 import Profile from "./pages/Profile";
+import Login from "./pages/Auth/Login"
+import SignUp from "./pages/Auth/register";
 function App() {
   return (
 
-      <div>
+    // <GoogleOAuthProvider clientId="470373993744-tjq6l6bk7ikvbvl46vpbd12pcqepuctb.apps.googleusercontent.com">     
         <Routes>
-          <Route path="/" element={<Profile />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
         </Routes>
+    // </GoogleOAuthProvider> 
 
-      </div>
   );
 }
 export default App;
--- frontend/src/components/UI/Logo.jsx
+++ frontend/src/components/UI/Logo.jsx
@@ -4,8 +4,7 @@ import clsx from 'clsx'
 export default function Logo({variant = 'navbar' })
 {
     return(
-        <a href="/login" target='_self' 
-            className="block mx-auto">
+
             <img src={LogoSvg}
                 alt="LeetPong logo"
                 className={clsx(
@@ -13,9 +12,8 @@ export default function Logo({variant = 'navbar' })
                     {
                         'flex-1 block mt-auto h-12 w-auto bg-white-600 justify-center' : variant === 'Logo',
                         'w-32 h-auto': variant === 'navbar',
-                        'w-82 h-82 ' : variant === 'Landing'
+                        'w-100 h-100 mb-30' : variant === 'Landing'
                     }
                 )}/>
-        </a>
     );
 }
