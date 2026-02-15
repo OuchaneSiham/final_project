9ad6acb38fef2c161a0566a37b9ef0270153c68f
changli2001 <changlichan2@gmail.com>
2026-02-15 11:04:05 +0000

    Adding Drop Down Box with styling

---
+++ frontend/src/components/Profile/ProfileDropdown.jsx
@@ -0,0 +1,10 @@
+
+
+export default function ProfileDropdown({className})
+{
+    return(
+        <div className={className}>
+                <p>ddsdsdsdsdd</p>
+        </div>
+    );
+}
--- frontend/src/components/Profile/ProfileHeader.jsx
+++ frontend/src/components/Profile/ProfileHeader.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import backArrow from '../../Assets/icons/backArrow.png'
 import face from '../../Assets/images/face.jpg'
+import ProfileDropdown from './ProfileDropdown'
+import { RiDashboardHorizontalLine } from "react-icons/ri";
+import { PiSignOutBold } from "react-icons/pi";
+
 
 export default function ProfileHeader()
 {
+    //DropDown state;
+    const [isDropdownOpen, setisDropdownOpen] = useState(true);
     return(
         <header className="flex justify-between items-center  w-full h-[60px] py-[8px] px-[12px]">
             <button type="button" className='w-[28px] h-[28px] opacity-100 hover:opacity-70 transition-opacity duration-300'>
@@ -13,11 +20,36 @@ export default function ProfileHeader()
             </div>
 
             {/* drop Down */}
-            <button type='button' className='cursor-pointer border-[#FFFFFF4D] border-[2px] rounded-full w-[36px] h-[36px] ease-in-out overflow-hidden '>
-                <img src={face}
-                        alt="the user avatar"
-                        className='w-full h-full object-cover'/>
-            </button>
+            <div className='relative'>
+                <button type='button'
+                        className='relative cursor-pointer border-[#FFFFFF4D] border-[2px] rounded-full w-[36px] h-[36px] ease-in-out overflow-hidden '
+                        onClick={() => setisDropdownOpen(prev => !prev)}
+                >
+                    <img src={face}
+                            alt="the user avatar"
+                            className='w-full h-full object-cover'
+                    />
+                </button>
+
+                        {isDropdownOpen && (
+                            <div className=' z-100 flex flex-col shadow-lg justify-center items-center absolute top-[100%] rounded-[12px] right-[50%] mt-[8px] bg-[#162D2A] w-[180px] border-[1px] border-[1px] border-[#FFFFFF26]'>
+                                <button type="button" className=' flex gap-2 cursor-pointer w-full inline-flex items-center h-[48px] px-[22px] p-[12px]'>
+                                    <RiDashboardHorizontalLine className="h-[20px] w-[20px] text-[#FFFFFF]"/>
+                                    <p className='text-[#FFFFFF]'>
+                                        Dashboard
+                                    </p>
+                                </button>
+                                <div class="border-b-1 border-black w-2/3 "></div>
+                                <button type="button" className='flex gap-2 cursor-pointer w-full inline-flex items-center h-[48px] px-[22px] py-[12px]'>
+                                    <PiSignOutBold className='h-[20px] w-[20px] text-[#FFFFFF]'/>
+                                    <p className='text-[16px] text-[#FFFFFF] '>
+                                        Sign out
+                                    </p>
+                                </button>                              
+                            </div>
+                            )
+                        }
+            </div>
         </header>
     );
 }
